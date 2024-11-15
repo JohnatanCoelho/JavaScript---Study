644ea342a8a4e776f9468914994e86d45f173363
Johnatan Coelho <johnatansouza242@gmail.com>
2024-11-15 12:07:27 -0300

    Inserindo aula Eventos de DOM

---
+++ Estudos por fora/javascript.js
@@ -0,0 +1,45 @@
+/* Class - aprendizado
+
+class formaDeBolo{
+	constructor(saborDaMassa, saborRecheio){
+    	this.saborDaMassa = saborDaMassa
+        this.saborRecheio = saborRecheio
+    }
+}
+
+let boloFesta = new formaDeBolo ("Massa de chocolate", "Recheio de nutella")
+
+console.log(boloFesta)
+
+
+OBS: "SEMPRE USAR CONSTRUCTOR - BOA PRÁTICA E FUNCIONA"
+
+    */
+
+
+
+
+/*TRABALHANDO COM MÉTODOS E PROPRIEDADES
+
+class formaDeBolo{
+	constructor(saborDaMassa, saborRecheio){
+    	this.saborDaMassa = saborDaMassa
+        this.saborRecheio = saborRecheio
+    }
+    escrever(){
+    	console.log(`UM delicioso bolo de ${this.saborDaMassa} com recheio de ${this.saborRecheio}`)
+    }
+    assar(){
+    	console.log(`Bolo assando de ${this.saborDaMassa}`)
+    }
+}
+
+let boloFesta = new formaDeBolo ("chocolate", " nutella")
+let boloPremium = new formaDeBolo("Baunilha", "coco")
+
+boloFesta.escrever()
+boloPremium.escrever()
+boloPremium.assar()
+
+*/
+   
